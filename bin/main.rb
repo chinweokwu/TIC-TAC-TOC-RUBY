ef7703ac04508b0b1503fbe5b6adcfc6c8d7ba21
chinwe <44407831+chinweokwu@users.noreply.github.com>
2020-04-27 23:38:17 +0100

    added more content to main file

--- bin/main.rb
+++ bin/main.rb
@@ -5,3 +5,15 @@ puts '--------'
 puts '  |  |  '
 puts '--------'
 puts '  |  |  '
+
+puts 'This is a game played by two players "X" & "O"'
+
+puts 'What is player-one name:'
+player1 = gets.chomp
+puts 'What is player_two name:'
+player2 = gets.chomp
+
+puts "#{player1}: where do you want to go? 0-8"
+puts "#{player1} place your token 'X'"
+puts "#{player2}: where do you want to go? 0-8"
+puts "#{player2} place your token 'O'"
