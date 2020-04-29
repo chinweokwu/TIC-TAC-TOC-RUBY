4dd0ea24c393d2b8b45c174ad8e8468c5d37b274
Muhammad Arslan <58504655+arslanbisharat@users.noreply.github.com>
2020-04-30 00:25:27 +0500

    rubocop errors removed

--- bin/main.rb
+++ bin/main.rb
@@ -1,24 +1,16 @@
 class TicTacToe
-
   def initialize
-
     greetings
 
     start_game
 
     play_again
-
   end
-
-
 
   # when the game is started we greet the players
 
   def greetings
-
     puts 'Hello! welcome to TIC-TAC-TOC game'
-
-
 
     puts '  |  |  '
 
@@ -43,32 +35,22 @@ class TicTacToe
     player2 = gets.chomp
 
     puts " Hi #{player1} & #{player2}"
-
   end
-
-
 
   #  After greeting the players we start the game
 
   def start_game
-
     # this would conatin the game logic
 
     puts 'game logic'
-
   end
-
-
 
   # we ask if the user wi=ould want to play again
 
   def play_again
-
     puts 'Would you love to play again? Y/N'
 
     result = gets.chomp.upcase
-
-
 
     if result == 'Y'
 
@@ -83,12 +65,9 @@ class TicTacToe
       puts 'Invalid inputs chose either Y/N'
 
     end
-
   end
-
 end
 
 # k = TicTacToe.new
 
 # k.intialize
-
