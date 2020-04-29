2f46964ae5d437639ab8f770303024fd98d78a03
Muhammad Arslan <58504655+arslanbisharat@users.noreply.github.com>
2020-04-30 01:06:14 +0500

    Code Updated According to the requirments

--- bin/main.rb
+++ bin/main.rb
@@ -1,73 +1,330 @@
 class TicTacToe
+
+
+
   def initialize
+
+
+
     greetings
 
-    start_game
 
-    play_again
+
+    game_loop
+
+
+
   end
+
+
+
+
+
+
 
   # when the game is started we greet the players
 
+
+
+
+
+
+
   def greetings
+
+
+
     puts 'Hello! welcome to TIC-TAC-TOC game'
 
+
+
+
+
+
+
     puts '  |  |  '
+
+
+
+
+
+
 
     puts '--------'
 
+
+
+
+
+
+
     puts '  |  |  '
+
+
+
+
+
+
 
     puts '--------'
 
+
+
+
+
+
+
     puts '  |  |  '
+
+
+
+
+
+
 
     puts 'This is a game played by two players "X" & "O"'
 
+
+
+
+
+
+
     puts 'Let start the game!'
+
+
+
+
+
+
 
     puts 'Enter player-one name:'
 
+
+
+
+
+
+
     player1 = gets.chomp
+
+
+
+
+
+
 
     puts 'What is player_two name:'
 
+
+
+
+
+
+
     player2 = gets.chomp
 
+
+
+
+
+
+
     puts " Hi #{player1} & #{player2}"
+
+
+
   end
+
+
+
+
+
+
 
   #  After greeting the players we start the game
 
-  def start_game
-    # this would conatin the game logic
 
-    puts 'game logic'
+
+
+
+
+
+  def start_game
+
+
+
+      # game logic
+
+
+
+      # game tells which player turn it is
+
+
+
+      # game asks player to select from available moves
+
+
+
+      # game informs player if selected move is invalid
+
+
+
+      # game displays board after player move
+
+
+
+      # game informs player if selected move is a winning move
+
+
+
+      # game informs player if selected move is a draw move
+
+
+
+      # game repeats all actions for next player's move
+
+
+
+end
+
+
+
+
+
+
+
   end
+
+
+
+
+
+
 
   # we ask if the user wi=ould want to play again
 
+
+
+
+
+
+
   def play_again
-    puts 'Would you love to play again? Y/N'
 
-    result = gets.chomp.upcase
 
-    if result == 'Y'
 
-      start_game
+    loop do
 
-    elsif result == 'N'
 
-      puts 'Thank you for playing'
 
-    else
+      puts 'Would you love to play again? Y/N'
 
-      puts 'Invalid inputs chose either Y/N'
+
+
+
+
+
+
+      result = gets.chomp.upcase
+
+
+
+
+
+
+
+      if result == 'Y'
+
+
+
+
+
+
+
+        start_game
+
+
+
+
+
+
+
+      elsif result == 'N'
+
+
+
+
+
+
+
+        puts 'Thank you for playing'
+
+
+
+
+
+
+
+      else
+
+
+
+
+
+
+
+        puts 'Invalid inputs chose either Y/N'
+
+
+
+
+
+
+
+      end
+
+
 
     end
+
+
+
   end
+
+
+
+
+
+
+
+  def game_loop
+
+
+
+    start_game while play_again
+
+
+
+  end
+
 end
+
+
+
+
+
+
 
 # k = TicTacToe.new
 
+
+
+
+
+
+
 # k.intialize
+
+
+
