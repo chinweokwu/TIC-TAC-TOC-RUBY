59065aa3e8b1408aeef4628849e69b604c7d6cb8
chinwe <44407831+chinweokwu@users.noreply.github.com>
2020-04-30 16:00:35 +0100

    made changes requested by tse

--- bin/main.rb
+++ bin/main.rb
@@ -1,330 +1,58 @@
 class TicTacToe
-
-
-
   def initialize
-
-
-
     greetings
-
-
-
     game_loop
-
-
-
   end
-
-
-
-
-
-
 
   # when the game is started we greet the players
-
-
-
-
-
-
-
   def greetings
-
-
-
     puts 'Hello! welcome to TIC-TAC-TOC game'
-
-
-
-
-
-
-
     puts '  |  |  '
-
-
-
-
-
-
-
     puts '--------'
-
-
-
-
-
-
-
     puts '  |  |  '
-
-
-
-
-
-
-
     puts '--------'
-
-
-
-
-
-
-
     puts '  |  |  '
-
-
-
-
-
-
-
     puts 'This is a game played by two players "X" & "O"'
-
-
-
-
-
-
-
     puts 'Let start the game!'
-
-
-
-
-
-
-
     puts 'Enter player-one name:'
-
-
-
-
-
-
-
     player1 = gets.chomp
-
-
-
-
-
-
-
     puts 'What is player_two name:'
-
-
-
-
-
-
-
     player2 = gets.chomp
-
-
-
-
-
-
-
     puts " Hi #{player1} & #{player2}"
-
-
-
   end
-
-
-
-
-
-
 
   #  After greeting the players we start the game
-
-
-
-
-
-
-
   def start_game
-
-
-
-      # game logic
-
-
-
-      # game tells which player turn it is
-
-
-
-      # game asks player to select from available moves
-
-
-
-      # game informs player if selected move is invalid
-
-
-
-      # game displays board after player move
-
-
-
-      # game informs player if selected move is a winning move
-
-
-
-      # game informs player if selected move is a draw move
-
-
-
-      # game repeats all actions for next player's move
-
-
-
-end
-
-
-
-
-
-
-
+    # game logic
+    puts 'game tells which player turn it is'
+    puts 'game asks player to select from available moves'
+    puts 'game informs player if selected move is invalid'
+    puts 'game displays board after player move'
+    puts 'game informs player if selected move is a winning move'
+    puts 'game informs player if selected move is a draw move'
+    puts "game repeats all actions for next player's move"
   end
-
-
-
-
-
-
-
-  # we ask if the user wi=ould want to play again
-
-
-
-
-
-
-
-  def play_again
-
-
-
-    loop do
-
-
-
-      puts 'Would you love to play again? Y/N'
-
-
-
-
-
-
-
-      result = gets.chomp.upcase
-
-
-
-
-
-
-
-      if result == 'Y'
-
-
-
-
-
-
-
-        start_game
-
-
-
-
-
-
-
-      elsif result == 'N'
-
-
-
-
-
-
-
-        puts 'Thank you for playing'
-
-
-
-
-
-
-
-      else
-
-
-
-
-
-
-
-        puts 'Invalid inputs chose either Y/N'
-
-
-
-
-
-
-
-      end
-
-
-
-    end
-
-
-
-  end
-
-
-
-
-
-
 
   def game_loop
-
-
-
-    start_game while play_again
-
-
-
+    start_game
+    play_again
   end
 
+  # we ask if the user wi=ould want to play again
+  def play_again 
+    result = ''
+    until result == 'Y' || result == 'N'
+      puts 'Would you like to play again? (Y/N): '
+      result = gets.chomp.upcase
+    end
+    case result
+    when 'Y'
+      start_game
+    when 'N'
+      puts 'Thank you for playing!'
+    end
+  end
 end
 
-
-
-
-
-
-
-# k = TicTacToe.new
-
-
-
-
-
-
-
-# k.intialize
-
-
-
+ttt = TicTacToe.new
+ttt.initialize
