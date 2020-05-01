4308afc5e8d87a61814d57d800f8aae8e21fab01
Muhammad Arslan <58504655+arslanbisharat@users.noreply.github.com>
2020-05-02 01:00:56 +0500

    spaces removed from board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,78 +1,39 @@
 class Board
-
   attr_accessor :Board
 
-
-
   def initialize
-
     @board = ['', '', '', '', '', '', '', '', '']
-
   end
 
-
-
   def display_board
-
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
 
-
-
     puts ' ----------- '
-
-
 
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
 
-
-
     puts ' ----------- '
 
-
-
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-
   end
-
-
 
   def player_move(index, token)
-
     @board[index] = token
-
   end
 
-
-
   def position_taken?(index)
-
     if (@board[index] == ' ') || (@board[index] == '') || @board[index].nil?
-
-
 
       false
 
-
-
     else
-
-
 
       true
 
-
-
     end
-
   end
-
-
 
   def valid_move?(index)
-
     true if index.between?(0, 8) && !position_taken?(@board, index)
-
   end
-
 end
-
