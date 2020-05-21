14457ac8e3478b66cca0492ebcaa7b4d1384e048
chinwe <44407831+chinweokwu@users.noreply.github.com>
2020-05-21 03:08:47 +0100

    validate player class

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,27 @@
 require_relative('./player.rb')
 require_relative('./board.rb')
 # rubocop: disable Style/DoubleNegation
-class TicTacToe
+class Name_Validation
+  def numeric?(string)
+    !!Kernel.Float(string)
+  rescue TypeError, ArgumentError
+    false
+  end
+  
+  def empty_string(strings)
+    true if (strings == ' ') || (strings == '') || strings.nil?
+  end
 
+  def same_name(team1,team2)
+    while team1 == team2
+      return 'please Enter different name:'
+    end
+  end
+
+  def name_valid(name_num,strings)
+    while numeric?(name_num) || empty_string(strings)
+    return 'Enter player name as string'
+    end
+  end
 end
-
 # rubocop: enable Style/DoubleNegation
