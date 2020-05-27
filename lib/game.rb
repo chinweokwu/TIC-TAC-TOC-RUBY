2ec018feca765d612cf568e77083d951ad6fd97d
Muhammad Arslan <thearslan380@gmail.com>
2020-05-27 09:20:41 -0700

    sticler error

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,19 @@
 require_relative('./player.rb')
 require_relative('./board.rb')
 # rubocop: disable Style/DoubleNegation
-class Name_Validation
+class Name_validation
   def numeric?(string)
     !!Kernel.Float(string)
   rescue TypeError, ArgumentError
     false
   end
-  
   def empty_string(strings)
     true if (strings == ' ') || (strings == '') || strings.nil?
   end
 
   def same_name(team1,team2)
     while team1 == team2
-      return 'please Enter different name:'
+      return 'please Enter different name:' 
     end
   end
 
@@ -22,6 +21,7 @@ class Name_Validation
     while numeric?(name_num) || empty_string(strings)
     return 'Enter player name as string'
     end
-  end
+  end  
 end
+
 # rubocop: enable Style/DoubleNegation
